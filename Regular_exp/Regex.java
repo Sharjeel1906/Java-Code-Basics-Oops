e7e0c68778c191391c0407f18e0a1b5c54fa89c2
Sharjeel1906 <sharjeelahmed1906@gmail.com>
2025-11-04 19:40:00 +0500

    new-entry

--- Regular_exp/Regex.java
+++ Regular_exp/Regex.java
@@ -7,9 +7,10 @@ public class Regex {
         // "[a]$" at end of string  || "[//?//.]" // remove actual meaning and find them as char  ||"[a-z]" all char in range
         // "[0-9]" all digit in raneg || "\\d" any digit || "\\d{2}" give 2 digit number || "\\d{2,4}" give 2,3 and 4 digit numbers
         // "\\d{4}|\\d{2}" give only 2 and 4 digit number || "\\d{2}-\\d{2}-\\d{4}" for 02-09-2025 || "\\d{1,2}-\\d{1,2}-\\d{4}" for 02-9-2025 2-09-2025
-        // "\\d{1,2}-\\d{1,2}-\\d{4}|\\d{1,2}-\\d{1,2}-\\d{2}" for all types of date
-        String whatToSearch = "";
-        String textToSearchIn = "This is a book of a maths publish on 02-09-2025 and 02-9-2025 and 2-9-2025 and 2-9-25  ";
+        // "\\d{1,2}-\\d{1,2}-\\d{4}|\\d{1,2}-\\d{1,2}-\\d{2}" for all types of date || u? means “u appears 0 or 1 time”.
+        // u* means 0 or more occurrance.
+        String whatToSearch = "[a]^";
+        String textToSearchIn = "This is a book of a maths publish on 02-09-2025 and 02-9-2025 and 2-9-2025 and 2-9-25  a";
 
         Pattern patternobj = Pattern.compile(whatToSearch);
         Matcher matcherobj = patternobj.matcher(textToSearchIn);
